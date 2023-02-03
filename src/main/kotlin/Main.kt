223dcc99d641afd0161f2e71424aa9299a046b7e
Rebeccanak <rebeccanakachwaaa@gmail.com>
2023-02-03 07:21:13 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+    var fullname = "Nakachwa Rebecca"
+    println(fullname)
+
+    var age = 24
+    println(age)
+
+    var phonenumber = 754597478
+    println(phonenumber)
+
+    var weightinkg: Byte = 56
+    println(weightinkg)
+
+    var citizen: Boolean = false
+    println(citizen)
+}
+
+
+
+
+
+
+
+
